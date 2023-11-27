de74932e316c40e7a0acd662da9df80a735c09c3
bportugal01 <bruno.portugal01@etec.sp.gov.br>
2023-11-27 11:54:59 -0300

    bancoCerto

---
+++ bdbeleza.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27/11/2023 às 15:54
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bdbeleza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.cliente: #1932 - Table &#039;bdbeleza.cliente&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.cliente: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`cliente`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itemnotafiscal`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.itemnotafiscal: #1932 - Table &#039;bdbeleza.itemnotafiscal&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.itemnotafiscal: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`itemnotafiscal`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `notafiscal`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.notafiscal: #1932 - Table &#039;bdbeleza.notafiscal&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.notafiscal: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`notafiscal`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pontoestrategico`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.pontoestrategico: #1932 - Table &#039;bdbeleza.pontoestrategico&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.pontoestrategico: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`pontoestrategico`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.produto: #1932 - Table &#039;bdbeleza.produto&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.produto: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`produto`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `regiao`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.regiao: #1932 - Table &#039;bdbeleza.regiao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.regiao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`regiao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `telefone`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.telefone: #1932 - Table &#039;bdbeleza.telefone&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.telefone: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`telefone`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tipotelefone`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.tipotelefone: #1932 - Table &#039;bdbeleza.tipotelefone&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.tipotelefone: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`tipotelefone`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `utilizacaoveiculo`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.utilizacaoveiculo: #1932 - Table &#039;bdbeleza.utilizacaoveiculo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.utilizacaoveiculo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`utilizacaoveiculo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `veiculo`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.veiculo: #1932 - Table &#039;bdbeleza.veiculo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.veiculo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`veiculo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendedor`
+--
+-- Erro ao ler a estrutura para a tabela bdbeleza.vendedor: #1932 - Table &#039;bdbeleza.vendedor&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela bdbeleza.vendedor: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bdbeleza`.`vendedor`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
